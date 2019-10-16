bce5fe2d833ff1dbf11a6074563709aba9f6c2de
Enchan <enchant0130@gmail.com>
2019-10-16 14:39:12 +0900

    [Update] import and  export cart

--- main.js
+++ main.js
@@ -1,9 +1,63 @@
 window.onload = function () {
-    //--カート画面では自動エクスポートし、必要に応じて出力
-    if(this.location.href = "http://akizukidenshi.com/catalog/cart/cart.aspx"){
-        let cart = cartExport();
 
-    }
+    //--かごリストを取得
+    let target = document.querySelector("body");
+    cartImport();
+
+    //--ボタン生成
+    let base = document.createElement("div");
+    let extd = document.createElement("span");
+    let exButton = document.createElement("button");
+    exButton.addEventListener( "click" , savecart , false );
+    exButton.innerHTML = "かごをエクスポートする";
+    extd.appendChild(exButton);
+    base.appendChild(extd);
+
+    let intd = document.createElement("span");
+    let inButton = document.createElement("button");
+    inButton.addEventListener( "click" , loadcart , false );
+    inButton.innerHTML = "かごをインポートする";
+    intd.appendChild(inButton);
+    base.appendChild(intd);
+
+    target.appendChild(base);
+}
+
+//--ロード/セーブ
+function savecart() {
+    let cart = cartExport();
+    localStorage.setItem("cart", cart);
+}
+function loadcart(){
+    let target = document.querySelector('input#importcartsubmit');
+    target.click();
+};
+
+//--保存した通販コードをインポート
+function cartImport(){
+    //localstorageから読んでくる
+    let cart = localStorage.getItem("cart");
+    if(cart == null) cart = "";
+
+    //--インポート用のフォームを作る
+    let ipForm = document.createElement("form");
+    ipForm.setAttribute("style", "display:none;");
+    ipForm.setAttribute("action", "/catalog/quickorder/blanketorder.aspx");
+    ipForm.setAttribute("name", "blanket_form");
+    ipForm.setAttribute("method", "post");
+    let ipInput = document.createElement("input");
+    ipInput.setAttribute("name", "blanket");
+    ipInput.setAttribute("value", "True");
+    let ipTArea = document.createElement("textarea");
+    ipTArea.setAttribute("name", "regist_goods");
+    ipTArea.innerHTML = cart;
+    let ipSubmit = document.createElement("input");
+    ipSubmit.setAttribute("id", "importcartsubmit");
+    ipSubmit.setAttribute("type", "submit");
+    ipForm.appendChild(ipInput);
+    ipForm.appendChild(ipTArea);
+    ipForm.appendChild(ipSubmit);
+    document.body.appendChild(ipForm);
 }
 
 //--通販コードをエクスポート
