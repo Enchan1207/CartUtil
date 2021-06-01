0cea21a5cdbd7916396cb0bf7a70a4c9fdbad8be
Enchan <enchant0130@gmail.com>
2021-06-01 23:09:44 +0900

    [Update #21] add option-page, load created wishlist

--- src/options/script.js
+++ src/options/script.js
@@ -1,3 +1,49 @@
 //
 // options script
 //
+
+import WishListManager from "../lib/WishListManager.js";
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const manager = new WishListManager();
+    await manager.restore();
+
+    const cellElements = manager.getLists().map((list) => {
+        const listtype = "aki"; // TODO: WishListクラスにサイトタイプを示すプロパティを埋め込む
+        const baseListElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
+        anchorElement.href = "#";
+        anchorElement.className = `list ${listtype}`;
+        const thumbnailElement = document.createElement("span");
+        thumbnailElement.className = "thumb";
+        const iconElement = document.createElement("img");
+        // TODO: こんな感じのを実装 またはThumbnailURLMakerを作成
+        // list.items[0].product.getThumbnailURL()
+        iconElement.src = "/assets/icons/icon.svg";
+
+        const infoElement = document.createElement("span");
+        infoElement.className = "info";
+
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = list.name;
+
+        const countElement = document.createElement("span");
+        countElement.className = "count";
+        countElement.textContent = `${list.items.length}個のアイテム`;
+
+        infoElement.appendChild(nameElement);
+        infoElement.appendChild(countElement);
+        thumbnailElement.appendChild(iconElement);
+        anchorElement.appendChild(thumbnailElement);
+        anchorElement.appendChild(infoElement);
+
+        baseListElement.appendChild(anchorElement);
+
+        return baseListElement;
+    });
+
+    cellElements.forEach((element) => {
+        const wishlistElement = document.getElementById("lists");
+        wishlistElement.appendChild(element);
+    });
+});
