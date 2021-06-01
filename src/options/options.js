0096f0df2c63aa0ea6e54a28130056d4f08e6652
Enchan <enchant0130@gmail.com>
2021-06-02 08:04:40 +0900

    [Update #21] wishlist details

--- src/options/options.js
+++ src/options/options.js
@@ -1,5 +1,5 @@
 //
-// options script
+// options script - ほしい物リスト一覧
 //
 
 import WishListManager from "../lib/WishListManager.js";
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const listtype = "aki"; // TODO: WishListクラスにサイトタイプを示すプロパティを埋め込む
         const baseListElement = document.createElement("li");
         const anchorElement = document.createElement("a");
-        anchorElement.href = "#";
+        anchorElement.href = `details.html?id=${list.id}`;
         anchorElement.className = `list ${listtype}`;
         const thumbnailElement = document.createElement("span");
         thumbnailElement.className = "thumb";
@@ -31,19 +31,27 @@ document.addEventListener('DOMContentLoaded', async () => {
         countElement.className = "count";
         countElement.textContent = `${list.items.length}個のアイテム`;
 
+        const contextMenuElement = document.createElement("span");
+        contextMenuElement.className = "contextmenu";
+        contextMenuElement.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#666" class="bi bi-three-dots" viewBox="0 0 16 16">
+                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+            </svg>`;
+
         infoElement.appendChild(nameElement);
         infoElement.appendChild(countElement);
         thumbnailElement.appendChild(iconElement);
         anchorElement.appendChild(thumbnailElement);
         anchorElement.appendChild(infoElement);
+        anchorElement.appendChild(contextMenuElement);
 
         baseListElement.appendChild(anchorElement);
 
         return baseListElement;
     });
 
+    const wishlistElement = document.getElementById("lists");
     cellElements.forEach((element) => {
-        const wishlistElement = document.getElementById("lists");
         wishlistElement.appendChild(element);
     });
 });
