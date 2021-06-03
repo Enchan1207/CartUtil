18f7ebb21c9e9f4c93a3ec38ddec225d3ad5ce59
Enchan <enchant0130@gmail.com>
2021-06-03 20:58:31 +0900

    [Update #21] add three-dots icon to asset/, modify options.js

--- src/options/options.js
+++ src/options/options.js
@@ -10,7 +10,10 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     const cellElements = manager.getLists().map((list) => {
         const listtype = "aki"; // TODO: WishListクラスにサイトタイプを示すプロパティを埋め込む
+
         const baseListElement = document.createElement("li");
+        baseListElement.setAttribute("data-listid", list.id);
+
         const anchorElement = document.createElement("a");
         anchorElement.href = `details.html?id=${list.id}`;
         anchorElement.className = `list ${listtype}`;
@@ -19,6 +22,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         const iconElement = document.createElement("img");
         // TODO: こんな感じのを実装 またはThumbnailURLMakerを作成
         // list.items[0].product.getThumbnailURL()
+        // いっけねスイッチサイエンスの画像UUID含んでるやん Productから逆算できない
+
+        // うーーーーんこれはもうProductにthumbnailsを追加するしかないかも
         iconElement.src = "/assets/icons/icon.svg";
 
         const infoElement = document.createElement("span");
@@ -33,10 +39,15 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         const contextMenuElement = document.createElement("span");
         contextMenuElement.className = "contextmenu";
-        contextMenuElement.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#666" class="bi bi-three-dots" viewBox="0 0 16 16">
-                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-            </svg>`;
+        contextMenuElement.addEventListener('click', (event) => {
+            console.log(baseListElement.getAttribute("data-listid"));
+            event.preventDefault();
+            event.stopPropagation();
+        });
+
+        const contextMenuIconElement = document.createElement("img");
+        contextMenuIconElement.src = "/assets/threedots.svg";
+        contextMenuElement.appendChild(contextMenuIconElement);
 
         infoElement.appendChild(nameElement);
         infoElement.appendChild(countElement);
